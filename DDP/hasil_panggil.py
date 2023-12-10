10b1b052a97f31a80ed0bde884afe5345e896d80
hanaapriliani13 <146050204+hanaapriliani13@users.noreply.github.com>
2023-12-10 23:53:02 +0700

    Add files via upload

---
+++ DDP/hasil_panggil.py
@@ -0,0 +1,21 @@
+from bangun_datar import *
+persegi (25)
+persegipanjang (25, 50)
+lingkaran (32)
+jajargenjang (5, 10, 20)
+layanglayang (32, 16, 8, 5)
+belahketupat (24, 30, 5)
+segitiga (4, 8, 4)
+
+
+from perhitungan import *
+tambah (2, 2)
+kurang (45, 9)
+kali (15, 15)
+bagi (35, 5)
+pangkat (18, 2)
+log (120)
+akar (9)
+sin (30)
+cos (130)
+tan (45)
